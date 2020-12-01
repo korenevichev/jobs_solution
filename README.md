62fe95ebca191732530d7bc75b70d99e1be1dd41
Lisa <korenevich.elizaveta@gmail.com>
2020-12-01 22:31:47 +0100

    Update seed file

--- README.md
+++ README.md
@@ -4,7 +4,6 @@ The setups steps expect following tools installed on the system.
 
 - Yarn
 - Ruby 2.6.0
-- Rails 6.0.3
 
 ##### 1. Check out the repository
 
@@ -12,12 +11,13 @@ The setups steps expect following tools installed on the system.
 git clone git@github.com:korenevichev/jobs_solution.git
 ```
 
-##### 2. Bundle install
+##### 2. Install dependencies
 
 Run the following command to install gems.
 
 ```ruby
 bundle install
+yarn install
 ```
 
 
@@ -26,16 +26,10 @@ bundle install
 Run the following commands to create and setup the database.
 
 ```ruby
-bundle exec rake db:create
 bundle exec rake db:setup
 ```
-##### 4. Populate database with data
 
-```ruby
-bundle exec rake db:seed
-```
-
-##### 5. Start the Rails server
+##### 4. Start the Rails server
 
 You can start the rails server using the command given below.
 
@@ -45,7 +39,7 @@ bundle exec rails s
 
 And now you can visit the site with the URL http://localhost:3000
 
-##### 6. Run tests
+##### 5. Run tests
 
 You can run tests  using the command given below.
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,97 +1,31 @@
-User.create!([
-  {email: "test_admin@mail.com", encrypted_password: "$2a$12$opXFlFG8H6XpTBkZ4rlma.B5U5vLixxX6n/dMHlpb.WIOdTdS2d66", reset_password_token: nil, reset_password_sent_at: nil, remember_created_at: nil, role_id: 1},
-  {email: "test_user@mail.com", encrypted_password: "$2a$12$9Ck8gB3utFNmv53Jciw9WeiZzcWiJ/6BUSjdTnkZ9IwRtK4jUCD8i", reset_password_token: nil, reset_password_sent_at: nil, remember_created_at: nil, role_id: 2}
-])
-JobsUser.create!([
-  {user_id: 5, job_id: 1},
-  {user_id: 5, job_id: 1},
-  {user_id: 6, job_id: 1}
-])
-User::HABTM_Jobs.create!([
-  {user_id: 5, job_id: 1},
-  {user_id: 5, job_id: 1},
-  {user_id: 6, job_id: 1}
-])
-Language.create!([
-  {name: "English"},
-  {name: "German"},
-  {name: "Spanish"},
-  {name: "Japanese"}
-])
-Language::HABTM_Jobs.create!([
-  {job_id: 1, language_id: 1},
-  {job_id: 1, language_id: 2},
-  {job_id: 10, language_id: 1},
-  {job_id: 10, language_id: 2},
-  {job_id: 10, language_id: 3},
-  {job_id: 11, language_id: 1},
-  {job_id: 11, language_id: 2},
-  {job_id: 11, language_id: 3},
-  {job_id: 12, language_id: 1},
-  {job_id: 14, language_id: 1},
-  {job_id: 15, language_id: 1},
-  {job_id: 16, language_id: 4},
-  {job_id: 17, language_id: 4},
-  {job_id: 18, language_id: 4}
-])
-Shift.create!([
-  {period: "[2020-11-26 07:00:00 UTC,2020-11-26 15:00:00 UTC]"},
-  {period: "[2020-11-27 07:00:00 UTC,2020-11-27 11:00:00 UTC]"},
-  {period: "[2020-11-27 11:00:00 UTC,2020-11-27 13:00:00 UTC]"},
-  {period: "[2020-11-27 15:00:00 UTC,2020-11-27 19:00:00 UTC]"},
-  {period: "[2020-11-28 07:00:00 UTC,2020-11-28 09:00:00 UTC]"},
-  {period: "[2020-11-28 09:00:00 UTC,2020-11-28 13:00:00 UTC]"},
-  {period: "[2020-11-28 11:00:00 UTC,2020-11-28 19:00:00 UTC]"},
-  {period: "[2020-11-29 11:00:00 UTC,2020-11-29 19:00:00 UTC]"}
-])
-Shift::HABTM_Jobs.create!([
-  {job_id: 15, shift_id: 3},
-  {job_id: 16, shift_id: 2},
-  {job_id: 16, shift_id: 3},
-  {job_id: 17, shift_id: 2},
-  {job_id: 1, shift_id: 14},
-  {job_id: 14, shift_id: 15},
-  {job_id: 14, shift_id: 17},
-  {job_id: 14, shift_id: 19},
-  {job_id: 18, shift_id: 16},
-  {job_id: 18, shift_id: 20},
-  {job_id: 18, shift_id: 21}
-])
-Role.create!([
-  {name: "admin"},
-  {name: "member"}
-])
-Job.create!([
-  {title: "Job 1", salary_per_hour: "10.0"},
-  {title: "Job 3", salary_per_hour: "20.0"},
-  {title: "Job 2", salary_per_hour: "8.0"}
-])
-Job::HABTM_Languages.create!([
-  {job_id: 1, language_id: 1},
-  {job_id: 1, language_id: 2},
-  {job_id: 10, language_id: 1},
-  {job_id: 10, language_id: 2},
-  {job_id: 10, language_id: 3},
-  {job_id: 11, language_id: 1},
-  {job_id: 11, language_id: 2},
-  {job_id: 11, language_id: 3},
-  {job_id: 12, language_id: 1},
-  {job_id: 14, language_id: 1},
-  {job_id: 15, language_id: 1},
-  {job_id: 16, language_id: 4},
-  {job_id: 17, language_id: 4},
-  {job_id: 18, language_id: 4}
-])
-Job::HABTM_Shifts.create!([
-  {job_id: 15, shift_id: 3},
-  {job_id: 16, shift_id: 2},
-  {job_id: 16, shift_id: 3},
-  {job_id: 17, shift_id: 2},
-  {job_id: 1, shift_id: 14},
-  {job_id: 14, shift_id: 15},
-  {job_id: 14, shift_id: 17},
-  {job_id: 14, shift_id: 19},
-  {job_id: 18, shift_id: 16},
-  {job_id: 18, shift_id: 20},
-  {job_id: 18, shift_id: 21}
-])
+ActiveRecord::Base.transaction do
+  Role.create!([
+     {id: 1, name: "admin"},
+     {id: 2, name: "member"}
+   ])
+  User.create!([
+    {email: "test_admin@mail.com", password: '123456', encrypted_password: "$2a$12$opXFlFG8H6XpTBkZ4rlma.B5U5vLixxX6n/dMHlpb.WIOdTdS2d66", reset_password_token: nil, reset_password_sent_at: nil, remember_created_at: nil, role_id: 1},
+    {email: "test_user@mail.com", password: '123456', encrypted_password: "$2a$12$9Ck8gB3utFNmv53Jciw9WeiZzcWiJ/6BUSjdTnkZ9IwRtK4jUCD8i", reset_password_token: nil, reset_password_sent_at: nil, remember_created_at: nil, role_id: 2}
+  ])
+  Shift.create!([
+    {id: 1, from: "2020-11-26 07:00:00 UTC", to: "2020-11-26 15:00:00 UTC"},
+    {id: 2, from: "2020-11-27 07:00:00 UTC", to: "2020-11-27 11:00:00 UTC"},
+    {id: 3, from: "2020-11-27 11:00:00 UTC", to: "2020-11-27 13:00:00 UTC"},
+    {id: 4, from: "2020-11-27 15:00:00 UTC", to: "2020-11-27 19:00:00 UTC"},
+    {id: 5, from: "2020-11-28 07:00:00 UTC", to: "2020-11-28 09:00:00 UTC"},
+    {id: 6, from: "2020-11-28 09:00:00 UTC", to: "2020-11-28 13:00:00 UTC"},
+    {id: 7, from: "2020-11-28 11:00:00 UTC", to: "2020-11-28 19:00:00 UTC"},
+    {id: 8, from: "2020-11-29 11:00:00 UTC", to: "2020-11-29 19:00:00 UTC"}
+  ])
+  Language.create!([
+    {id: 1, name: "English"},
+    {id: 2, name: "German"},
+    {id: 3, name: "Spanish"},
+    {id: 4, name: "Japanese"}
+  ])
+  Job.create!([
+    {id: 1, title: "Job 1", salary_per_hour: "10.0", languages: Language.where(id: [1, 2]), shifts: Shift.where(id: [1])},
+    {id: 2, title: "Job 2", salary_per_hour: "20.0", languages: Language.where(id: [1]), shifts: Shift.where(id: [2,5,6,8])},
+    {id: 3, title: "Job 3", salary_per_hour: "8.0", languages: Language.where(id: [3, 4]), shifts: Shift.where(id: [4,7])}
+  ])
+end
